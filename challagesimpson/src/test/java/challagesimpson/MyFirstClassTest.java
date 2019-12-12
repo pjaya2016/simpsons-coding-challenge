41023fb9d01988a79b97d7677ad7df00099c9e09
pjaya <pjaya@LAPTOP-57VIH3T6.home>
2019-12-12 11:34:25 +0000

    Added Unit Test

---
+++ challagesimpson/src/test/java/challagesimpson/MyFirstClassTest.java
@@ -0,0 +1,18 @@
+package challagesimpson;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import io.jsonwebtoken.lang.Assert;
+
+public class MyFirstClassTest {
+
+	@Test
+	public void MyFirstMethod() 
+	{
+		String t = "Test";
+		 assertEquals(t, t, "First test");
+		
+	}
+}
---
+++ challagesimpson/src/test/java/challagesimpson/MyTestRunner.java
@@ -0,0 +1,14 @@
+package challagesimpson;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class MyTestRunner {
+  public static void main(String[] args) {
+    Result result = JUnitCore.runClasses(MyFirstClassTest.class);
+    for (Failure failure : result.getFailures()) {
+      System.out.println(failure.toString());
+    }
+  }
+}
